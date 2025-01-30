5bc702ab051b81bc50865b271751feef04c0fc67
Hamid Reza Khavarie <hrp.develop@gmail.com>
2025-01-30 16:06:30 +0430

    Update production environment API URL comment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://employeewebapi-eyf4arhac9escxgh.uaenorth-01.azurewebsites.net'  // Azure production URL
+  apiUrl: 'https://employeewebapi-eyf4arhac9escxgh.uaenorth-01.azurewebsites.net'  // Azure backend URL
 }; 
